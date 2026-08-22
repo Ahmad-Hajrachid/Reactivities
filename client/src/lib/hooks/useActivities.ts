38a0ae2336595235f6731cb40c8b2aa92dc6d000
AhmadSafi2025 <ahmad@safitechs.com>
2026-08-22 22:57:33 +0300

    End of section 9

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,15 +1,21 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import type { Activity } from "../../classes/Activity";
 import agent from "../api/agent";
+import { useLocation } from "react-router";
 
 export const useActivities = (id? :string)=>{
     const queryClient = useQueryClient();
+    const location = useLocation();
+
     const {data: activities, isPending} = useQuery({
     queryKey : ['activities'],
     queryFn: async ()=>{
       const response = await agent.get<Activity[]>("/activities");
       return response.data;
-    }
+    },
+    enabled: !id  && location.pathname === '/activities'
+    //refetches after this timer ends
+    // staleTime: 1000 * 60 * 5
   });
 
   const {data : activity, isLoading: isLoadingActivity} = useQuery({
